5bd3c0b44092e9d1c7743eece1f4fcf9f576276d
Tu Nombre <tu-email@ejemplo.com>
2026-07-24 19:21:35 +0200

    Implementa rutas HTTP de clientes

--- apps/api/src/customers/customers.controller.ts
+++ apps/api/src/customers/customers.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CustomersService } from './customers.service';
 
-@Controller('customers')
-export class CustomersController {}
+@Controller('companies/:companyId/customers')
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Post()
+  create(
+    @Param('companyId', new ParseUUIDPipe({ version: '4' }))
+    companyId: string,
+    @Body() data: CreateCustomerDto,
+  ) {
+    return this.customersService.create(companyId, data);
+  }
+
+  @Get()
+  findAll(
+    @Param('companyId', new ParseUUIDPipe({ version: '4' }))
+    companyId: string,
+  ) {
+    return this.customersService.findAll(companyId);
+  }
+
+  @Get(':customerId')
+  findOne(
+    @Param('companyId', new ParseUUIDPipe({ version: '4' }))
+    companyId: string,
+    @Param('customerId', new ParseUUIDPipe({ version: '4' }))
+    customerId: string,
+  ) {
+    return this.customersService.findOne(companyId, customerId);
+  }
+}
